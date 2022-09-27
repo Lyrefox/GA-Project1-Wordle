637a1d37fa43e43d2a482e76b33769467c8c5b55
Matthew Lea <matthewlea8@gmail.com>
2022-09-27 18:06:45 +1000

    added basic enter functionality, backspace half working

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,3 +1,8 @@
 for (key of keyboardButton) {
     key.addEventListener('click', keyboardClick)
 }
+document.addEventListener('keypress', (event) => {
+    const keyPress = event.key;
+    // keyboardClick(keyPress)
+    console.log(keyPress)
+})
--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,23 @@
 const keyboardButton = document.getElementsByClassName('keyboard-button')
 let colNum = 0
-
+let rowNum = 0
 function keyboardClick(event) {
     const clickedKey = event.target
-    console.log(clickedKey)
+    console.log(typeof(clickedKey))
     const keyPressed = clickedKey.dataset.key
     console.log(keyPressed)
     const rowEntry = document.getElementsByClassName('c'+colNum)
-    if (colNum < 5) {
-        rowEntry[0].innerText = keyPressed
+    if (colNum <= 5 && keyPressed !== 'backspace' && keyPressed !== 'enter') {
+        rowEntry[rowNum].innerText = keyPressed
         colNum2 = colNum++
         console.log(colNum)
-    }if (keyPressed === 'backspace') {
-        colNum--
-        rowEntry[0].InnerText = ''
+    }if (keyPressed === 'backspace' && colNum >= 0) {
+        rowEntry[rowNum].innerText = ''
+        colNum2 = colNum--
         console.log(colNum)
+    }if (keyPressed === 'enter' && colNum === 5) {
+        rowNum++
+        colNum = 0
     }
     
     
