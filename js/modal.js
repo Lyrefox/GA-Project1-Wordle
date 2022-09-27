cca8d00495a731a5d7872f91e163b796fc8bf699
Matthew Lea <matthewlea8@gmail.com>
2022-09-27 18:52:40 +1000

    added array that pushes and pops elements based on current visual

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,8 @@
 const keyboardButton = document.getElementsByClassName('keyboard-button')
+let userEntry = []
 let colNum = 0
 let rowNum = 0
-function keyboardClick(event) {
+function keyboardClick(event) { //function for onscreen keyboard
     const clickedKey = event.target
     console.log(clickedKey)
     const keyPressed = clickedKey.dataset.key
@@ -10,9 +11,13 @@ function keyboardClick(event) {
     if (colNum <= 5 && keyPressed !== 'backspace' && keyPressed !== 'enter') {
         rowEntry[rowNum].innerText = keyPressed
         colNum++
+        userEntry.push(keyPressed)
+        console.log(userEntry)
         console.log(colNum)
     }if (keyPressed === 'backspace' && colNum >= 0) {
         colNum--
+        userEntry.pop()
+        console.log(userEntry)
         // console.log(colNum)
         rowEntry[rowNum].innerText = ''
         if (colNum === -1) {
@@ -25,14 +30,18 @@ function keyboardClick(event) {
     }
 }
 
-function keyboardPress(keyPress) {
+function keyboardPress(keyPress) { //function for physical keyboard
     const rowEntry = document.getElementsByClassName('c'+colNum)
     if (colNum <= 5 && keyPress !== 'Backspace' && keyPress !== 'Enter') { // need to work out how to ignore everything that isnt a letter or backspace and enter
         rowEntry[rowNum].innerText = keyPress
         colNum++
+        userEntry.push(keyPress)
+        console.log(userEntry)
         console.log(colNum)
     }if (keyPress === 'Backspace' && colNum >= 0) {
         colNum--
+        userEntry.pop()
+        console.log(userEntry)
         // console.log(colNum)
         rowEntry[rowNum].innerText = ''
         if (colNum === -1) {
